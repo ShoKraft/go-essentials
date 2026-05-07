d40dc666af7183a83e9d19b6c7e4821a8751c43f
Shohrat Ereshov <shohrat.ereshov@gmail.com>
2026-05-07 14:35:45 +0500

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	// types.TypesExample()
+	types.TypesExample()
 	bit_tricks.FastModPowerOfTwo(25, 3)
 	bit_tricks.FastModPowerOfTwo(100, 4)
 	fmt.Println(constatns.FormatBytes(1125899906842624))
